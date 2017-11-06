bc4956f1fca254e5d8ae629fa455931fac6bd959
Ryley Ameden <rameden@dyn.com>
2017-11-06 16:25:28 -0500

    nav toggle icon

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,12 @@
 <header class="app-header banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"alt="<?php bloginfo('name'); ?>"></a>
-      <a class="sidebar-toggler visible-lg" href="#">test</a>
-      <a class="mobile-sidebar-toggler visible-sm" href="#">test</a>
+    <button class="sidebar-toggler visible-lg" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <button class="mobile-sidebar-toggler visible-sm" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
   </div>
 
 </header>
